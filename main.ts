3a7f88de673643343824c2e0da6496e2a3fed52a
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2020-01-27 22:40:13 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,33 @@
+//% weight=10 color=#008B00 icon="\uf0c2" block="sharpGP2Y1010AU0F"
+namespace sharpGP2Y1010AU0F {
 
+    let Reference_VOLTAGE = 3100
+
+    /**
+     * get dust value (μg/m³) 
+     * @param vLED describe parameter here, eg: DigitalPin.P16
+     * @param vo describe parameter here, eg: AnalogPin.P1
+     */
+    //% blockId="readdust" block="value of dust(μg/m³) at LED %vLED| out %vo"
+    export function ReadDust(vLED: DigitalPin, vo: AnalogPin): number {
+        let voltage = 0;
+        let dust = 0;
+        pins.digitalWritePin(vLED, 0);
+        control.waitMicros(160);
+        voltage = pins.analogReadPin(vo);
+        control.waitMicros(100);
+        pins.digitalWritePin(vLED, 1);
+        voltage = pins.map(
+            voltage,
+            0,
+            1023,
+            0,
+            Reference_VOLTAGE / 2 * 3
+        );
+        dust = (voltage - 380) * 5 / 29;
+        if (dust < 0) {
+            dust = 0
+        }
+        return Math.round(dust)
+    }
+}
